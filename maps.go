925a94d619731966163dadccc4902c4bc501c88a
oggy107 <urmalsingh10@gmail.com>
2023-08-17 22:06:51 +0530

    added: notes

---
+++ maps.go
@@ -0,0 +1,30 @@
+package main
+
+/**
+func main() {
+	// ages := map[string]int{
+	// 	"john":  19,
+	// 	"james": 23,
+	// }
+
+	ages := make(map[string]int)
+	ages["john"] = 87
+	ages["smith"] = 69
+
+	fmt.Println(ages)
+	fmt.Println(len(ages))
+
+	// inserting
+	ages["oggy"] = 22
+
+	// getting
+	// age := ages["smith"]
+
+	// deleting
+	delete(ages, "smith")
+
+	// check if key exists
+	s, ok := ages["smith"]
+	fmt.Println(s, ok)
+}
+**/
